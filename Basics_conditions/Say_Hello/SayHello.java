b4f80e7f54340938134cfbe5ac744752c0916a6a
Karim Ahmed <karim.ahmed14770@gmail.com>
2025-12-07 11:13:07 +0200

    Java Solution

---
+++ Basics_conditions/Say_Hello/SayHello.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+ 
+public class SayHello{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.print("Hello, "+name);
+    }
+}
